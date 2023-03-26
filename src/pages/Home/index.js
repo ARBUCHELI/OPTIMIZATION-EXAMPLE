92e604d3232f4549deab855f4cd9a0ab132e0bf9
ARBUCHELI <radio_seattle@hotmail.com>
2023-03-26 01:22:49 -0500

    editing home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,31 +4,31 @@ import './styles.css';
 export const Home = () => {
   return (
     <div className='container'>
-      <h1>Advanced React: Optimization Lesson</h1>
+      <h1>Advanced React: Optimization Examples</h1>
       <p className='home-description'>
-        Get started by clicking the links to each exercise:
+        Get started by clicking the links to each example:
       </p>
       <ul className='home-list'>
-        <li>Exercise 1: Introduction</li>
+        <li>Example 1: Introduction</li>
         <li>
-          <Link to='/exercises/2'>Exercise 2: Memoizing Values</Link>
+          <Link to='/exercises/2'>Example 2: Memoizing Values</Link>
         </li>
         <li>
-          <Link to='/exercises/3'>Exercise 3: Memoizing Components</Link>
+          <Link to='/exercises/3'>Example 3: Memoizing Components</Link>
         </li>
         <li>
-          <Link to='/exercises/4'>Exercise 4: Memoizing Functions</Link>
+          <Link to='/exercises/4'>Example 4: Memoizing Functions</Link>
         </li>
         <li>
-          <Link to='/exercises/5'>Exercise 5: Code Splitting Modules</Link>
+          <Link to='/exercises/5'>Example 5: Code Splitting Modules</Link>
         </li>
         <li>
-          <Link to='/exercises/6'>Exercise 6: Code Splitting Components</Link>
+          <Link to='/exercises/6'>Example 6: Code Splitting Components</Link>
         </li>
         <li>
-          <Link to='/exercises/7'>Exercise 7: Suspense</Link>
+          <Link to='/exercises/7'>Example 7: Suspense</Link>
         </li>
-        <li>Exercise 8: Review</li>
+        <li>Example 8: Review</li>
       </ul>
     </div>
   );
